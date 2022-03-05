4a6ea4bfc8614703c644798b0524627c9285d002
Jeppe Beier <jeppevinkel@gmail.com>
2022-03-06 00:58:33 +0100

    Export enums

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import {SteamSearch} from "./steamSearch";
 import {QueryBuilder} from "./queryBuilder";
+import {SortBy} from "./enums/sortBy";
+import {MaxPrice} from "./enums/maxPrice";
 
-export {SteamSearch, QueryBuilder};
+export {SteamSearch, QueryBuilder, SortBy, MaxPrice};
